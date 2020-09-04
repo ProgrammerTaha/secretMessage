6956cb55e0ef1bcc78187a375555c4d6c63745e2
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 21:38:59 +0300

    initial comment

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+
+todo: transform an array of strings into a secret message!
+
+*/
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what',
+  'you', 'get', 'easily', 'the', 'first', 'time,', 'it',
+  'is', 'about', 'what', 'you', 'can', 'figure', 'out.',
+  '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
